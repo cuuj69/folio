331a2643514c3dab7a3b5cd0020ba39299c79100
cuuj69 <mensahjefferson69@gmail.com>
2022-12-01 13:38:13 +0000

    Added darkmode feature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -205,3 +205,16 @@ $(document).ready(function() {
         project_close();
     });
 });
+
+
+
+
+$( ".change" ).on("click" , function() {
+    if( $ ( "body" ).hasClass( "dark" )) {
+        $( "body" ).removeClass( "dark" );
+        $( ".change" ).text( "OFF" );   
+    }else{
+        $( "body" ).addClass( " dark");
+        $( ".change").text( "ON");
+    }
+});
